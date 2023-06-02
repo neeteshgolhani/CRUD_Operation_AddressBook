3950be556412944bf44b9ed0b1cb44c30b88ff0d
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-06-02 16:37:40 +0530

    Added:Update contact

--- src/main/java/com/jdbc/AddressBookCRUD.java
+++ src/main/java/com/jdbc/AddressBookCRUD.java
@@ -11,34 +11,33 @@ public class AddressBookCRUD {
     public static void main(String[] args) {
         // Step 1: Establish a database connection
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-            // Step 2: Retrieve contacts (Read operation)
-            retrieveContacts(connection);
+            // Step 2: Update a contact (Update operation)
+            updateContact(connection, 6, "Rohit", "Sam", "Main street", "Gurgao", "Punjab", "323232", "+91 13323-4567", "Rohit.sam@example.com");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private static void retrieveContacts(Connection connection) throws SQLException {
-        String sql = "SELECT * FROM address_book";
+    private static void updateContact(Connection connection, int id, String firstName, String lastName, String address, String city, String state, String zip, String phone, String email) throws SQLException {
+        String sql = "UPDATE address_book SET first_name=?, last_name=?, address=?, city=?, state=?, zip=?, phone=?, email=? WHERE id=?";
 
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-            System.out.println("Contacts:");
-            while (resultSet.next()) {
-                // Retrieve the data for each contact from the ResultSet
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, "Amit");
+            statement.setString(2, "Ar");
+            statement.setString(3, "ABC road line");
+            statement.setString(4, "Jalandhar");
+            statement.setString(5, "UP");
+            statement.setString(6, "456456");
+            statement.setString(7, "91222333333");
+            statement.setString(8, "AMIT@GMAIL.COM");
+            statement.setInt(9, 6);
 
-                int id = resultSet.getInt("id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String address = resultSet.getString("address");
-                String city = resultSet.getString("city");
-                String state = resultSet.getString("state");
-                String zip = resultSet.getString("zip");
-                String phone = resultSet.getString("phone");
-                String email = resultSet.getString("email");
-                // Print the details of each contact
+            int rowsUpdated = statement.executeUpdate();
 
-                System.out.println("ID: " + id + ", Name: " + firstName + " " + lastName + ", Address: " + address + ", City: " + city + ", State: " + state + ", Zip: " + zip + ", Phone: " + phone + ", Email: " + email);
+            if (rowsUpdated > 0) {
+                System.out.println("Contact updated successfully.");
+            } else {
+                System.out.println("Contact not found or update failed.");
             }
         }
     }
